ba33f43b5a4ac152b3feaaecfb9cbefe66c944d2
Bradford Quinn <bradford.quinn.727@gmail.com>
2020-11-10 17:48:13 -0500

    incomplete, pushing to secure deploy key

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ import { fifaData } from './fifa.js';
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Investigate the data above. Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
+const finals2014 = fifaData.filter(function(item){
+    return item.Year === 2014 && item.Stage === 'Final';
+});
 
+console.log(finals2014);
 //(a) Home Team name for 2014 world cup final
-
+console.log('Task 1(a):', finals2014[0]['Home Team Name']);
 //(b) Away Team name for 2014 world cup final
-
+console.log()
 //(c) Home Team goals for 2014 world cup final
 
 //(d) Away Team goals for 2014 world cup final
